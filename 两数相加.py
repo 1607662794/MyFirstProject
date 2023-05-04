fb713b6a03cec4b91f70569783af03e1464aa427
憨憨菜 <1607662794@qq.com>
2023-05-04 20:26:02 +0800

    今天就这样吧，等我明天把链表形式的求和整出来

--- 两数相加.py
+++ 两数相加.py
@@ -1,9 +1,12 @@
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 '''第一种方法基于列表实现'''
+
+
 class Solution_list(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -14,7 +17,7 @@ class Solution_list(object):
         # 使用递归进行不断地拆分求和
         l = []
         # 肯定得至少一位数字相加，不然没有了意义
-        num = l1.pop() + l2.pop()
+        num = l1.pop(0) + l2.pop(0)
         l.append(num % 10)
         # 注意，空列表并不为None值，因此需要提取其布尔值进行运算
         # 这个循环判断排除掉了其余的三种情况
@@ -30,10 +33,16 @@ class Solution_list(object):
                     tag = 1
                 else:
                     tag = 0
-                num = l1.pop() + l2.pop() + tag
+                num = l1.pop(0) + l2.pop(0) + tag
                 l.append(num % 10)
-
+        if num > 9:
+            l.append(1)
         return l
+
+
+'''第二种方法基于链表实现'''
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -42,28 +51,43 @@ class Solution(object):
         :rtype: ListNode
         """
         # 使用递归进行不断地拆分求和
-        l = []
+
         # 肯定得至少一位数字相加，不然没有了意义
-        num = l1.pop() + l2.pop()
-        l.append(num % 10)
+        num = l1.val + l2.val
+        l = ListNode(num % 10, None)
         # 注意，空列表并不为None值，因此需要提取其布尔值进行运算
         # 这个循环判断排除掉了其余的三种情况
-        while (l1 == l2 and bool(l1) is False) is False:
+        while (l1 == l2 and l1 == None) is False:
             # 对不同位数字相加所做的准备
-            if bool(l1) is False and bool(l2) is not False:
-                l1.append(0)
-            elif bool(l2) is False and bool(l1) is not False:
-                l2.append(0)
+            if l1 == None and l2 != None:
+                l1.next(0)
+            elif l2 == None and l1 != None:
+                l2.next(0)
             else:
                 # 构造进位和不进位的操作
                 if num > 9:
                     tag = 1
                 else:
                     tag = 0
-                num = l1.pop() + l2.pop() + tag
-                l.append(num % 10)
+                num = l1.val + l2.val + tag
+                l.next(num % 10)
+
+            if num > 9:
+                l.next(1)
+            return l
 
         return l
 
-l1 = [2, 4]
-l2 = [5, 6, 4]
+
+
+
+node3 = ListNode(3,None)
+node2 = ListNode(4,node3)
+node1 = ListNode(2,node2)
+node6 = ListNode(4,None)
+node5 = ListNode(6,node6)
+node4 = ListNode(5,node5)
+
+print(node1.val)
+a = Solution()
+print(a.addTwoNumbers(node1, node4))
