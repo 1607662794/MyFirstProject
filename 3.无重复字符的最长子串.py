87371d33435c9682def9d87f93475e970727e1aa
憨憨菜 <1607662794@qq.com>
2023-05-07 11:04:36 +0800

    三题Over

--- 3.无重复字符的最长子串.py
+++ 3.无重复字符的最长子串.py
@@ -1,6 +1,7 @@
 class Solution(object):
     '''给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。'''
 
+    "重点是滑动窗口概念的运用"
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -18,6 +19,7 @@ class Solution(object):
                 length = max(length, len(partion_s))
 
             else:
+                #当然也可以用一个指标来进行切片，这样损失一点儿空间，但是极大地提高了时间效率
                 # 这种情况下，最大最字符串长度肯定是减的，所以不需要寻找其中的最大值
                 partion_s = partion_s.split(first_str)[-1]+first_str
 
