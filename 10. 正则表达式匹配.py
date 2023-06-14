7ad08069386cba1fdf4cff5c4ca57b8af5fcaa0b
憨憨菜 <1607662794@qq.com>
2023-06-14 22:20:40 +0800

    第十题，正则表达式尝试

--- 10. 正则表达式匹配.py
+++ 10. 正则表达式匹配.py
@@ -5,18 +5,28 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        if '.' not in p and '*'not in p:
-            if s!=p:
+        # 第一种情况，
+        if '*' not in p:
+            if len(p) != len(s):
                 return False
-        if p == '.*':
-            return True
-        if '.' in p and '*' not in p:
-            if len(s) == len(p):
-                for i in len(s):
+            else:
+                for i in range(len(s)):
                     if p[i]!='.':
                         if p[i]!=s[i]:
                             return False
                 return True
-            else:
-                return False
-        if '.' not in p and '*' in p:
+        else:
+            # tag_1 = False
+            # tag_2 = False
+
+            for i in p:
+                for j in s:
+                    '''为了使用滑动窗口，依次对比而设置的一些参数'''
+                    if i == '.':
+                        tag_1 = True#记录‘.’
+                    elif j == '*':
+                        tag_2 = True
+                    else:
+                        tag_1,tag_2 = False,False
+                        tag_3 = i
+                    if i != '.':
