234d4d5faf2e3ca27708d0bfa86fcd66d629f495
憨憨菜 <1607662794@qq.com>
2023-06-24 09:45:41 +0800

    第十题

--- 11. 盛最多水的容器.py
+++ 11. 盛最多水的容器.py
@@ -5,11 +5,25 @@ class Solution(object):
         :rtype: int
         """
         '''一个新的思路，双重循环解决，但是超时版'''
+        # max_area = 0
+        # for i in range(len(height)):
+        #     for j in range(i + 1, len(height)):
+        #         if min(height[i], height[j]) * (j - i) > max_area:
+        #             max_area = min(height[i], height[j]) * (j - i)
+        # return max_area
+        '''借鉴别人的思路，两个指针分别位于列表两侧，通过不断向内行走，寻找面积最大值；当前的两边，更改大边肯定无用，因为面积不是有它控制的，只能更改小边，就是多元函数的思想'''
         max_area = 0
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                if min(height[i], height[j]) * (j - i) > max_area:
-                    max_area = min(height[i], height[j]) * (j - i)
+        i = 0
+        j = len(height) - 1
+        while i != j:
+            if min(height[i], height[j]) * (j - i) > max_area:
+                max_area = min(height[i], height[j]) * (j - i)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
         return max_area
+
+
 a = Solution()
-print(a.maxArea([1,8,6,2,5,4,8,3,7]))
+print(a.maxArea([2, 3, 4, 5, 18, 17, 6]))
