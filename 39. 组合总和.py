e6c640dd27ad92f18579757fb1de2babb0eb71a0
憨憨菜 <1607662794@qq.com>
2023-12-28 10:51:24 +0800

    39. 组合总和

--- 39. 组合总和.py
+++ 39. 组合总和.py
@@ -9,7 +9,7 @@ class Solution(object):
         dp = {i: [] for i in range(target + 1)}  # 定义一个空字典，可以当哈希使用，dp是动态规划的简称。
 
         # 这里一定要将candidates降序排列
-        for i in sorted(candidates, reverse=True):  # 倒序查找的原因在于：没有漏的，也没有多于加的。顺序查找的话，会有重复值。
+        for i in sorted(candidates, reverse=True):  # 倒序查找的原因在于：没有漏的，也没有多于加的。顺序查找的话，会有重复值。有了最大的数可以求什么，有了次大的数可以求什么，不断往下走。
             for j in range(i, target + 1):
                 if j == i:  # 如果有了的话，那么先给每个字典或者哈希将本身放进去
                     dp[j] = [[i]]
