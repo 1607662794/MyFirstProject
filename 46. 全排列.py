ba5b82fe371883125f74ae308bf604786e772865
憨憨菜 <1607662794@qq.com>
2024-01-02 23:07:14 +0800

    46. 全排列

---
+++ 46. 全排列.py
@@ -0,0 +1,23 @@
+# 计划采用回溯法进行全排列的组合
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def dfs(x):
+            if x == len(nums) - 1:
+                res.append(list(nums))  # 添加排列方案
+                return
+            for i in range(x, len(nums)):
+                nums[i], nums[x] = nums[x], nums[i]  # 交换，将 nums[i] 固定在第 x 位
+                dfs(x + 1)  # 开启固定第 x + 1 位元素
+                nums[i], nums[x] = nums[x], nums[i]  # 恢复交换
+
+        res = []
+        dfs(0)
+        return res
+
+
+solution = Solution()
+print(solution.permute([1, 2, 3]))
