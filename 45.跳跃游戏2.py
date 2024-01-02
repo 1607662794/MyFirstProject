680bfa272e0a0f4b2935eaaaa845394a57e6ee6e
憨憨菜 <1607662794@qq.com>
2024-01-02 17:37:33 +0800

    45. 跳跃游戏 II
    广度优先搜索&贪心算法

--- 45.跳跃游戏2.py
+++ 45.跳跃游戏2.py
@@ -5,24 +5,42 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if nums == []:
+        if len(nums) <= 1:
             return 0
-        target = 0  # 看看当前走了几步
-        bfs = [nums[0]]  # 存放当前步数下的列表索引
-        while True:
-            for i in bfs:
-                bfs_1 = []
-                for j in range(nums[i]):
-                    if i + j + 1 == len(nums):
-                        return target
-                    elif i + j + 1 > len(nums):
-                        continue
-                    else:
-                        if i + j + 1 not in bfs_1:
-                            bfs_1.append(target + j + 1)
-            bfs = bfs_1
-            target = target + 1
+        elif nums[0] >= len(nums) - 1:
+            return 1
+        # # 接下来这段代码实现的是广度优先算法，但是在面临极大的计算算例是会出现时间超限的问题。
+        # step = 0  # 看看当前走了几步
+        # current_index = [0]  # 存放当前步数下的列表索引
+        # while True:
+        #     step = step + 1
+        #     current_index_1 = []
+        #     for i in current_index:
+        #         for j in range(nums[i], 0, -1):
+        #             if i + j == len(nums) - 1:
+        #                 return step
+        #             elif i + j > len(nums) - 1:
+        #                 continue
+        #             else:
+        #                 if i + j not in current_index_1 and i+j not in current_index:#剪枝操作：对于在当前步数下，存储的索引不必重复搜索，对于当前步数下计算出来的索引之前出现过，也不必重复索引，肯定不是最短路径
+        #                     current_index_1.append(i + j)
+        #     current_index = current_index_1
 
+        # 因此接下来这段代码实现的是深度优先算法。不能使用深度优先算法，因为它不一定是最短的步数。
+        # 看到贪心算法后，想到了一种新的解法，最后的目标值肯定能够到达，所采取的方式即为在当前步下，所能到达的最远值，索引+数值。
+        
+        current_index = 0
+        step = 0
+        while True:
+            max = nums[current_index]
+            for i in range(1, nums[current_index] + 1):
+                if current_index + i + nums[current_index + i] >= len(nums)-1:
+                    return step + 2
+                elif current_index + i + nums[current_index + i] > max:
+                    max = current_index + i + nums[current_index + i]
+                    max_index = current_index + i
+            current_index = max_index
+            step += 1
 
 solution = Solution()
-print(solution.jump([2,3,0,1,4]))
+print(solution.jump([1,1,1,1]))
