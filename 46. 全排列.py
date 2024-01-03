6c368d957497cd6f6efba6f51028dd8f4864a847
憨憨菜 <1607662794@qq.com>
2024-01-03 09:50:42 +0800

    46. 全排列

--- 46. 全排列.py
+++ 46. 全排列.py
@@ -1,4 +1,4 @@
-# 计划采用回溯法进行全排列的组合
+# 计划采用回溯法进行全排列的组合,回溯法就是带了剪枝操作的深度优先搜索。
 class Solution(object):
     def permute(self, nums):
         """
@@ -9,7 +9,7 @@ class Solution(object):
             if x == len(nums) - 1:
                 res.append(list(nums))  # 添加排列方案
                 return
-            for i in range(x, len(nums)):
+            for i in range(x, len(nums)):#此时固定了x-1位数，其余的分别放在x位上一次，这样就形成了全排列。
                 nums[i], nums[x] = nums[x], nums[i]  # 交换，将 nums[i] 固定在第 x 位
                 dfs(x + 1)  # 开启固定第 x + 1 位元素
                 nums[i], nums[x] = nums[x], nums[i]  # 恢复交换
@@ -17,7 +17,6 @@ class Solution(object):
         res = []
         dfs(0)
         return res
-
 
 solution = Solution()
 print(solution.permute([1, 2, 3]))
