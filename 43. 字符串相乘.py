40f73e22cae72b946e1766a7131a44319e6aea24
憨憨菜 <1607662794@qq.com>
2024-03-04 17:48:10 +0800

    43. 字符串相乘

---
+++ 43. 字符串相乘.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+        result_multiply = str(int(num1) * int(num2))
+        return result_multiply
+solution = Solution()
+print(solution.multiply("12","12"))
+
