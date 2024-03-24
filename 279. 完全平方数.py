95ae53e817f9ca7c8828c1dcbce1afbbf4f0ec36
憨憨菜 <1607662794@qq.com>
2024-03-24 16:24:45 +0800

    279. 完全平方数

---
+++ 279. 完全平方数.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        '''版本一，先遍历背包, 再遍历物品'''
+        # 自底向上
+        # 初始化
+        nums = [i ** 2 for i in range(1, n + 1) if i ** 2 <= n]
+        dp = [n] * (n + 1)
+        dp[0] = 0
+        # 遍历背包
+        for j in range(1, n + 1):
+            # 遍历物品
+            for num in nums:
+                if j >= num:
+                    dp[j] = min(dp[j], dp[j - num] + 1)
+        return dp[n]
+
+
+solution = Solution()
+print(solution.numSquares(12))
