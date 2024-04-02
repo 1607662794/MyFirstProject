c279d7d10fc47b16c9532c63e7de1975dac5797c
憨憨菜 <1607662794@qq.com>
2024-04-02 19:45:15 +0800

    416. 分割等和子集

--- 416. 分割等和子集.py
+++ 416. 分割等和子集.py
@@ -4,16 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # 0-1背包问题
-        target = sum(nums)
-        if target % 2 == 1: return False
-        target //= 2
-        dp = [0] * (len(nums) + 1)
-        for i in range(len(nums)):
-            for j in range(target, nums[i] - 1, -1):
-                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i])  # 对应不装入得情况，和装入的情况
-        return target == dp[target]
+        # 将这个问题转换为0-1背包问题
+        if sum(nums) % 2 != 0:
+            return False
+        else:
+            target = sum(nums) // 2
+            result = [[0 for i in range(target + 1)] for j in range(len(nums) + 1)]
+            for i in range(1, len(nums) + 1):
+                for j in range(1, target + 1):
+                    if j >= nums[i-1]:
+                        result[i][j] = max(result[i - 1][j], nums[i-1] + result[i - 1][j - nums[i-1]])  # 取最大值即塞满的情况
+                        if result[i][j] == target:
+                            return True
+                    else:
+                        result[i][j] = result[i - 1][j]
+            return False
+
+
 
 
 solution = Solution()
-print(solution.canPartition([2, 2, 1, 1]))
+print(solution.canPartition([1,5,10,6]))
