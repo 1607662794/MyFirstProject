64e083faa98ead2683e26dbe7acc2fe3a6814c5e
憨憨菜 <1607662794@qq.com>
2024-04-24 08:48:35 +0800

    70. 爬楼梯

--- 70. 爬楼梯-法2动态规划.py
+++ 70. 爬楼梯-法2动态规划.py
@@ -6,19 +6,27 @@ class Solution(object):
         :rtype: int
         """
 
-        def dp(n, memo):
-            if n == 1:
-                return 1
-            elif n == 2:
-                return 2
-            elif memo[n] != 0:
-                return memo[n]
+        # def dp(n, memo):
+        #     if n == 1:
+        #         return 1
+        #     elif n == 2:
+        #         return 2
+        #     elif memo[n] != 0:
+        #         return memo[n]
+        #     else:
+        #         memo[n] = dp(n - 1, memo) + dp(n - 2, memo)
+        #         return memo[n]
+        #
+        # return dp(n, [0] * (n + 1))
+        method = [0 for i in range(n+1)]
+        for i in range(1,n+1):
+            if i == 1:
+                method[i] = 1
+            elif i == 2:
+                method[i] = 2
             else:
-                memo[n] = dp(n - 1, memo) + dp(n - 2, memo)
-                return memo[n]
-
-        return dp(n, [0] * (n + 1))
-
+                method[i] = method[i-1]+method[i-2]
+        return method[-1]
 
 solution = Solution()
-print(solution.climbStairs(2))
+print(solution.climbStairs(3))
